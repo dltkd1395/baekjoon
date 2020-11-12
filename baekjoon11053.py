c51f6c5d69b5855f5651193fc00aa09790346a6e
dltkd1395 <39490416+dltkd1395@users.noreply.github.com>
2020-11-12 14:06:55 +0900

    baekjoon

---
+++ baekjoon11053.py
@@ -0,0 +1,13 @@
+# 백준[11053]
+
+n=int(input())
+a=list(map(int, input().split()))
+
+dp=[1 for _ in range(n)]
+
+for i in range(n):
+  for j in range(i):
+    if a[i]>a[j]:
+      dp[i]=max(dp[i],dp[j]+1)
+    
+print(max(dp))
