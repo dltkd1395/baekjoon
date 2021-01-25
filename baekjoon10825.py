ee6303661308495b4ed78bef6eeffadebd6e99fe
dltkd1395 <39490416+dltkd1395@users.noreply.github.com>
2021-01-25 20:10:00 +0900

    baekjoon

---
+++ baekjoon10825.py
@@ -0,0 +1,12 @@
+# 백준[10825번]
+
+n=int(input())
+data=[]
+for i in range(n):
+  a,b,c,d=input().split()
+  data.append([a,int(b),int(c),int(d)])
+
+data.sort(key=lambda x:(-x[1],x[2],-x[3],x[0]))
+
+for x in data:
+  print(x[0])
