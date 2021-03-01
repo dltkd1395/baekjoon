26fb4f94dac16aafd4bf91ae9c52ed880ec7c9ee
dltkd1395 <39490416+dltkd1395@users.noreply.github.com>
2021-03-01 12:11:32 +0900

    baekjoon

---
+++ baekjoon1967.py
@@ -0,0 +1,34 @@
+# 백준[1967번]
+
+v=int(input())
+node_graph=[[] for _ in range(v+1)]
+
+for _ in range(v):
+  path=list(map(int,input().split()))
+
+  for i in range(1,len(path)//2):
+    node_graph[path[0]].append([path[2*i-1],path[2*i]])
+
+result_first=[0 for _ in range(v+1)]
+
+def DFS(start_node, now_node, matrix, result):
+  for x,y in matrix[now_node]:
+    if result[x]==0 and start_node!=x:
+      result[x]=result[now_node]+y
+      DFS(now_node, x, matrix, result)
+
+DFS(1,1,node_graph,result_first)
+result_final[1]=0
+
+tmp=0
+index=0
+
+for i in range(len(result_first)):
+  if result_first[i]>tmp:
+    tmp=result_first[i]
+    index=i
+
+result_final=[0 for _ in range(v+1)]
+DFS(index, index, node_graph, result_final)
+result_final[index]=0
+print(max(result_final))
